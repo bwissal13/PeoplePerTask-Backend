8b42290cf52883af783f9cd5aa869ebfdaa43ca3
bwissal <baazizwissal13@gmail.com>
2023-11-20 09:53:21 +0100

    insert data in tables

--- sql.sql
+++ sql.sql
@@ -44,3 +44,10 @@ CREATE TABLE Testimonials (
     Testimonial_ID INT PRIMARY KEY,
     Comment TEXT  
 );
+INSERT INTO categories VALUES (1,'Programming'),(2,'Graphic Design'),(3,'Writing'),(4,'Data Entry'),(5,'Translation');
+INSERT INTO freelances VALUES (1,'Web Developer Freelance','fr',1),(2,'Graphic Designer Freelance','ar',2),(3,'Copywriter Freelance','fr',3),(4,'Data Analyst Freelance','ar',4),(5,'Translator Freelance','fr',5);
+INSERT INTO projets VALUES (1,'E-commerce Website Development','Build a fully functional e-commerce website.',1,1,1),(2,'Company Logo Redesign','Redesign the company logo for a modern look.',2,2,2),(3,'Content Marketing Strategy','Develop a content marketing strategy for the next quarter.',3,3,3),(4,'Data Visualization Dashboard','Create a dashboard for visualizing company data.',4,4,4),(5,'Translate Product Descriptions','Translate product descriptions into multiple languages.',5,5,5);
+INSERT INTO souscategories VALUES (1,'Web Development',1),(2,'Logo Design',2),(3,'Copywriting',3),(4,'Data Analysis',4),(5,'Language Translation',5);
+INSERT INTO users VALUES (1,'khalid ','hashed_password_1','khalid.waheli@email.com','weird'),(2,'Abdeljlil','hashed_password_2','abdo.sno7i@email.com','weird'),(3,'hamza','hashed_password_3','hamza.elazzhar@email.com','weird'),(4,'zakaria','hashed_password_4','zakaria.elkoh@email.com','weird'),(5,'fati','hashed_password_5','fatima.ezzahra@email.com','cute');
+INSERT INTO testimonials VALUES (1,'Great freelancer! Delivered the project ahead of schedule.'),(2,'Excellent work, very professional and reliable.'),(3,'Impressed with the quality of service. Will hire again.'),(4,'Quick and efficient. Highly recommended.'),(5,'Good communication and attention to detail.');
+INSERT INTO offres VALUES (1,500,7),(2,300,5),(3,800,10),(4,400,6),(5,600,8);
